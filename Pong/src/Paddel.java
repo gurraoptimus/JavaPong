f1e27211b7a3a17eceebaf6ae891d691de52a10f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-10-25 22:44:07 +0200

    Java

--- Pong/src/Paddel.java
+++ Pong/src/Paddel.java
@@ -5,6 +5,19 @@ import javax.swing.*;
 
 public class Paddel{
     Paddel(){
+
+    }
+    public void KeyPressed(KeyEvent e){
+            
+    }
+    public void KeyReleased(KeyEvent e){
+            
+    }
+    public void setYDirection(int yDirection){
+
+    }
+    public void move(){
         
     }
+
 }
