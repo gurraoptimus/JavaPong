0ec6853fd5264b6e4f3434e8d05d7ceb4275a99d
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-10-27 17:20:15 +0200

    java

--- Pong/src/GamePanel.java
+++ Pong/src/GamePanel.java
@@ -15,7 +15,7 @@ public class GamePanel extends JPanel implements Runnable{
     public void newBall(){
 
     }
-    public void newPaddels(){
+    public void newPaddles(){
 
     }
     public void paint(Graphics g){
