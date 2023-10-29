9463d93c22fb8a51a2e2154aa947310c631556d8
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-10-29 23:18:54 +0100

    java

--- Pong/src/GamePanel.java
+++ Pong/src/GamePanel.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class GamePanel extends JPanel implements Runnable{
     static final int GAME_WIDTH=1000;
-    static final int GAME_HEIGHT=(int)(GAME_WIDTH * (5/9));
+    static final int GAME_HEIGHT=(int)(GAME_WIDTH * (0.5555));
     static final Dimension SCREEN_SIZE=new Dimension(GAME_WIDTH,GAME_HEIGHT);
     static final int BALL_DIAMETER=20;
     static final int PADDLE_WIDTH=25;
@@ -25,7 +25,10 @@ public class GamePanel extends JPanel implements Runnable{
         score=new Score(GAME_WIDTH,GAME_HEIGHT);
         this.setFocusable(true)
         this.addKeyListener(new AL());
-        this.setP
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread=new Thread(this);
+        gameThread.start();
     }
     public void newBall(){
 
