1bd9deb6ab177b486bfdc0be8b1a947bf2d66861
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-02 14:40:54 +0100

    java

--- Pong/src/GamePanel.java
+++ Pong/src/GamePanel.java
@@ -54,7 +54,13 @@ public class GamePanel extends JPanel implements Runnable{
         //stops paddles at window edges
         if(paddle1.y<=0)
             paddle1.y=0;
-        if(paddle1.y >=())
+        if(paddle1.y >=(GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle1.y=GAME_HEIGHT-PADDLE_HEIGHT;
+
+        if(paddle2.y<=0)
+            paddle2.y=0;
+        if(paddle2.y >=(GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle2.y=GAME_HEIGHT-PADDLE_HEIGHT;
     }
     public void run(){
         //game loop
