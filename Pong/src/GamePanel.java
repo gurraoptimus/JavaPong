76e6f2c39cf86d1934eddcf8426919e55cf8250c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-05 12:46:53 +0100

    java

--- Pong/src/GamePanel.java
+++ Pong/src/GamePanel.java
@@ -95,6 +95,20 @@ public class GamePanel extends JPanel implements Runnable{
             paddle2.y=0;
         if(paddle2.y >=(GAME_HEIGHT-PADDLE_HEIGHT))
             paddle2.y=GAME_HEIGHT-PADDLE_HEIGHT;
+        //give a player 1 point and creates new paddles & ball
+		if(ball.x <=0) {
+			score.player2++;
+			newPaddles();
+			newBall();
+			System.out.println("Player 2: "+score.player2);
+		}
+		if(ball.x >= GAME_WIDTH-BALL_DIAMETER) {
+			score.player1++;
+			newPaddles();
+			newBall();
+			System.out.println("Player 1: "+score.player1);
+		}
+	}
     }
     public void run(){
         //game loop
