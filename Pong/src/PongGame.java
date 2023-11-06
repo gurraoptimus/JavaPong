72b5f6f9138c3ce11bf72ab1f612310e711e860e
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-06 12:07:32 +0100

    java

--- Pong/src/PongGame.java
+++ Pong/src/PongGame.java
@@ -5,6 +5,6 @@ import javax.swing.*;
 
 public class PongGame{
     public static void main(String[]args){
-        GameFrame frame=new GameFrame();
+        GameFrame frame = new GameFrame();
     }
 }
